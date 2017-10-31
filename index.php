d8cad3be7dc697882bfc0bbb9264de050a69b058
mg171 <mg171@hdm-stuttgart.de>
2017-10-31 12:14:50 +0100

    Neue Version

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="system/design/design.css" media="screen"/>
+</head>
+<body>
+<div class="topnav" id="myTopnav">
+    <a href="#home">Logo</a>
+    <a href="#news">Suche</a>
+    <a href="#contact">Warenkorb</a>
+    <a href="#about">Anmelden</a>
+</div>
 
 
-echo "Hallo Welt. Das ist ein neuer Test! Wie sieht es jetzt aus?";
 
-?>
+
+</body>
