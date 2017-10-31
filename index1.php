8b486c75ef4c9b91cfdf3dd4b7cc17f9c7f24932
mg171 <mg171@hdm-stuttgart.de>
2017-10-31 17:55:27 +0100

    Neue Version

--- index1.php
+++ index1.php
@@ -11,10 +11,48 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-<h1>Hello, world!</h1>
+
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Handy Webshop</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav col-md-12">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<h1 class="font-weight-bold text-center">Ein Shop alle Marken!</h1>
+<br>
+<br>
 
 <div class="container">
-    <p> Das hier ist ein neuer Text, der ganz bestimmt total komisch ausieht!</p>
+    <div class="row">
+        <div class="col-md-2">
+            <img src="jpg/lg.jpg">
+</div>
+    </div>
+
+
+
+
+
+
+
+
 </div>
 
 <!-- Optional JavaScript -->
