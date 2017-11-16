7ea69f5ea473de1ea5b31ba8d1a13973b15071a0
monique-omm <monique-omm@users.noreply.github.com>
2017-11-16 16:37:06 +0100

    Update and rename index1.php to index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Onlineshop</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen"/>
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <script src="https://use.fontawesome.com/2ab6c8da3b.js"></script>
+</head>
+
+<body>
+
+
+<br>
+<div class="navbar">
+
+    <?php
+
+        echo '<img src="images/logo/logo_navbar.jpg" alt="Logo">';
+        echo '<a href="index.php">Startseite</a>';
+        echo '<a href="index.php?page=login&action=login"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>';
+
+   
+    ?>
+
+
+</div>
+
+
+<?php
+
+if (isset($_GET["page"]) ) {
+    switch ($_GET["page"]) {
+        case "category":
+            include "category/index.php";
+            break;
+        default:
+            include "start.php";
+            break;
+    }
+}
+else
+{
+    include "start.php";
+}
+
+
+?>
+
+
+
+</body>
+
+<br><br><footer class="footer">Made @HdM by Lena, Marcel & Monique</footer>
+
+</html>
