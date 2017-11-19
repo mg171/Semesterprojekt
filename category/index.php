20da215df960747de6e03ad3e4a1b8101ab14f60
monique-omm <monique.omm@gmail.com>
2017-11-19 15:11:19 +0100

    category pages

---
+++ category/index.php
@@ -0,0 +1,31 @@
+<?php
+if (isset($_GET["category"]))
+{
+    switch ($_GET["category"]) {
+        case "apple":
+            include "show.php";
+            break;
+        case "samsung":
+            include "show.php";
+            break;
+        case "google":
+            include "show.php";
+            break;
+        case "huawei":
+            include "show.php";
+            break;
+        case "motorola":
+            include "show.php";
+            break;
+        case "lg":
+            include "show.php";
+            break;
+        default:
+            echo "Die Seite wurde nicht gefunden";
+            die();
+            break;
+    }
+} else {
+    echo "Die Seite wurde nicht gefunden";
+}
+?>
---
+++ category/show.php
@@ -0,0 +1,159 @@
+<div id="brandwrapper">
+
+<?php
+
+/* Ausgabe Apple */
+
+if ($_GET["category"] == "apple") {
+
+    echo "<h1>iPhones</h1>";
+
+    include_once("db/userdata.php");
+
+    $pdo = new PDO($dsn, $dbuser, $dbpass);
+
+    $sql = "SELECT * FROM produkt WHERE marke_id = 1";
+    foreach ($pdo->query($sql) as $row) {
+
+        $bildpfad = $row['bild'];
+
+        echo "<div class='hovereffect' id='grid'>
+        <img id='singlephone' src='images/productshots/" . $bildpfad . "' alt='Produktbild'>
+            <div class='overlay'><p>"
+            . $row['artikelname'] . "<br>" . $row['preis'] . "€" . "</p>";
+        echo "</div>";
+        echo "</div>";
+    }
+}
+
+
+/* Ausgabe Samsung */
+
+if ($_GET["category"] == "samsung") {
+
+    echo "<h1>Smartphones von Samsung</h1>";
+
+    include_once("db/userdata.php");
+
+    $pdo = new PDO($dsn, $dbuser, $dbpass);
+
+    $sql = "SELECT * FROM produkt WHERE marke_id = 2";
+    foreach ($pdo->query($sql) as $row) {
+
+        $bildpfad = $row['bild'];
+
+        echo "<div class='hovereffect' id='grid'>
+        <img id='singlephone' src='images/productshots/" . $bildpfad . "' alt='Produktbild'>
+            <div class='overlay'><p>"
+            . $row['artikelname'] . "<br>" . $row['preis'] . "€" . "</p>";
+        echo "</div>";
+        echo "</div>";
+    }
+
+}
+
+
+/* Ausgabe Google */
+
+if ($_GET["category"] == "google") {
+
+    echo "<h1>Smartphones von Google</h1>";
+
+    include_once("db/userdata.php");
+
+    $pdo = new PDO($dsn, $dbuser, $dbpass);
+
+    $sql = "SELECT * FROM produkt WHERE marke_id = 3";
+    foreach ($pdo->query($sql) as $row) {
+
+        $bildpfad = $row['bild'];
+
+        echo "<div class='hovereffect' id='grid'>
+        <img id='singlephone' src='images/productshots/" . $bildpfad . "' alt='Produktbild'>
+            <div class='overlay'><p>"
+            . $row['artikelname'] . "<br>" . $row['preis'] . "€" . "</p>";
+        echo "</div>";
+        echo "</div>";
+    }
+
+}
+
+
+/* Ausgabe Huawei */
+
+if ($_GET["category"] == "huawei") {
+
+    echo "<h1>Smartphones von Huawei</h1>";
+
+    include_once("db/userdata.php");
+
+    $pdo = new PDO($dsn, $dbuser, $dbpass);
+
+    $sql = "SELECT * FROM produkt WHERE marke_id = 4";
+    foreach ($pdo->query($sql) as $row) {
+
+        $bildpfad = $row['bild'];
+
+        echo "<div class='hovereffect' id='grid'>
+        <img id='singlephone' src='images/productshots/" . $bildpfad . "' alt='Produktbild'>
+            <div class='overlay'><p>"
+            . $row['artikelname'] . "<br>" . $row['preis'] . "€" . "</p>";
+        echo "</div>";
+        echo "</div>";
+    }
+
+}
+
+
+/* Ausgabe Motorola */
+
+if ($_GET["category"] == "motorola") {
+
+    echo "<h1>Smartphones von Motorola</h1>";
+
+    include_once("db/userdata.php");
+
+    $pdo = new PDO($dsn, $dbuser, $dbpass);
+
+    $sql = "SELECT * FROM produkt WHERE marke_id = 5";
+    foreach ($pdo->query($sql) as $row) {
+
+        $bildpfad = $row['bild'];
+
+        echo "<div class='hovereffect' id='grid'>
+        <img id='singlephone' src='images/productshots/" . $bildpfad . "' alt='Produktbild'>
+            <div class='overlay'><p>"
+            . $row['artikelname'] . "<br>" . $row['preis'] . "€" . "</p>";
+        echo "</div>";
+        echo "</div>";
+    }
+
+}
+
+
+/* Ausgabe LG */
+
+if ($_GET["category"] == "lg") {
+
+    echo "<h1>Smartphones von LG</h1>";
+
+    include_once("db/userdata.php");
+
+    $pdo = new PDO($dsn, $dbuser, $dbpass);
+
+    $sql = "SELECT * FROM produkt WHERE marke_id = 6";
+    foreach ($pdo->query($sql) as $row) {
+
+        $bildpfad = $row['bild'];
+
+        echo "<div class='hovereffect' id='grid'>
+        <img id='singlephone' src='images/productshots/" . $bildpfad . "' alt='Produktbild'>
+            <div class='overlay'><p>"
+            . $row['artikelname'] . "<br>" . $row['preis'] . "€" . "</p>";
+        echo "</div>";
+        echo "</div>";
+    }
+
+} ?>
+
+</div>
