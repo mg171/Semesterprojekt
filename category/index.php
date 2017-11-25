cac230d53150ba2d552e89028ac91b328a236a61
monique-omm <monique-omm@users.noreply.github.com>
2017-11-25 10:53:42 +0100

    Update index.php

--- category/index.php
+++ category/index.php
@@ -21,11 +21,11 @@ if (isset($_GET["category"]))
             include "show.php";
             break;
         default:
-            echo "Die Seite wurde nicht gefunden";
+            echo "Die Seite wurde nicht gefunden.";
             die();
             break;
     }
 } else {
-    echo "Die Seite wurde nicht gefunden";
+    echo "Die Seite existiert nicht.";
 }
 ?>
