b3747df08560aab7e9fd5295494ffd15cbc9356e
monique-omm <monique-omm@users.noreply.github.com>
2017-12-16 22:26:11 +0100

    Update update.php

--- warenkorb/update.php
+++ warenkorb/update.php
@@ -1,1 +1,14 @@
 <?php
+
+if($_POST["update"]) {
+    $prod_id = $_POST['prod_id'];
+    $new_qty = $_POST['quantity'];
+    foreach( $prod_id as $key => $id ) {
+        $_SESSION["warenkorb"][$id] = $new_qty[$key];
+    }
+}
+
+if($_POST["checkout"]) {
+    include_once('warenkorb/checkout.php');
+
+}
