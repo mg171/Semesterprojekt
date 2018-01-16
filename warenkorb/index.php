eb4d4d6161075b5283fc2f857daa0229b1a6e5c1
monique-omm <monique-omm@users.noreply.github.com>
2018-01-16 18:56:04 +0100

    Update index.php
    Price

--- warenkorb/index.php
+++ warenkorb/index.php
@@ -1,59 +1,59 @@
 <div id='cartwrapper'>
 
-<?php
-session_start();
-include_once ('db/userdata.php');
-if (isset($_SESSION['warenkorb']))
-{
-    ?>
-    <div>
-        <form action="./warenkorb/update.php" method="post">
-            <table>
-                <tr>
-                    <th>Id</th>
-                    <th>Produkt</th>
-                    <th>Preis</th>
-                    <th>Anzahl</th>
-                    <th>Löschen</th>
-                </tr>
-
-                <?php
-                $db = new PDO($dsn, $dbuser, $dbpass);
-                foreach ($_SESSION['warenkorb'] as $key => $cart){
-
-                    $stmt = $db->query("SELECT * FROM produkt WHERE id='".$cart['id']."'");
-                    $result = $stmt->fetch();
-                    ?>
+    <?php
+    session_start();
+    include_once ('db/userdata.php');
+    if (isset($_SESSION['warenkorb']))
+    {
+        ?>
+        <div>
+            <form action="./warenkorb/update.php" method="post">
+                <table>
                     <tr>
-                        <td><?=$key;?></td>
-                        <td><a href="?page=product&id=<?php echo $result['id']?>">
-                                <?=$result['artikelname'];?>
-                            </a></td>
-                        <td><?=$result['preis'];?></td>
-                        <td><?=$cart['anzahl'];?></td>
-                        <td><input type="checkbox" name="loeschen[]" value="<?=$key;?>"></td>
+                        <th>Produkt</th>
+                        <th>Preis</th>
+                        <th>Anzahl</th>
+                        <th>Gesamt</th>
+                        <th>Löschen</th>
                     </tr>
-                <?php }
-                ?>
-            </table>
-            <input type="submit" value="aktualisieren">
+
+                    <?php
+                    $db = new PDO($dsn, $dbuser, $dbpass);
+                    foreach ($_SESSION['warenkorb'] as $key => $cart){
+
+                        $stmt = $db->query("SELECT * FROM produkt WHERE id='".$cart['id']."'");
+                        $result = $stmt->fetch();
+                        ?>
+                        <tr>
+                            <td><a href="?page=product&product=show&id=<?php echo $result['id']?>">
+                                    <?=$result['artikelname'];?>
+                                </a></td>
+                            <td><?=$result['preis'].'€';?></td>
+                            <td><?=$cart['anzahl'];?></td>
+                            <td><?=$result['preis']*$cart['anzahl'].'€';?></td>
+                            <td><input type="checkbox" name="loeschen[]" value="<?=$key;?>"></td>
+                        </tr>
+                    <?php }
+                    ?>
+                </table>
+                <input type="submit" value="aktualisieren">
+            </form>
+        </div>
+
+        <form action="?page=checkout" method="post">
+            <input type="submit" value="Zur Kasse">
         </form>
-    </div>
 
-    <form action="?page=checkout" method="post">
-        <input type="submit" value="Zur Kasse">
-    </form>
+    <?php }
+    else
+    { ?>
+        <div>
+            <h2>Keine Artikel im Warenkorb</h2><br>
 
-<?php }
-else
-{ ?>
-    <div>
-        <h2>Keine Artikel im Warenkorb</h2><br>
+            <form>
+                <input type="button" value="zurück zur Startseite" onclick="window.location.href='?'"/>
+            </form>
+        </div>
 
-        <form>
-            <input type="button" value="zurück zur Startseite" onclick="window.location.href='?'"/>
-        </form>
-    </div>
-
-<?php } ?>
-    </div>
+    <?php } ?>
+</div>
