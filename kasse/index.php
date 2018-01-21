18bb8bf57bd0178ab19f3816a62181f7ff2df990
monique-omm <monique.omm@gmail.com>
2018-01-21 13:53:08 +0100

    Kasse

---
+++ kasse/index.php
@@ -0,0 +1,19 @@
+<?php
+if (isset($_GET["action"]))
+{
+    switch ($_GET["action"]) {
+        case "overview":
+            include "./kasse/zurkasse.php";
+            break;
+        case "erfolg":
+            include "./kasse/erfolg.php";
+            break;
+        default:
+            echo "Seite nicht gefunden";
+            die();
+            break;
+    }
+}
+else
+{
+    echo "Seite nicht gefunden";}
