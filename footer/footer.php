00b22e08a0758796b3308a0b0fdb25eec2b457ab
mg171 <mg171@hdm-stuttgart.de>
2018-01-31 17:14:21 +0100

    Neuer Footer 1

---
+++ footer/footer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+
+<div id="footer">
+
+    <table class="footertable">
+        <td>
+            <a style="color: white" href="impressum/impressum.html">| Impressum |</a>
+        </td>
+        <td>
+            <a style="color: white" href="agb/agb.html">| AGB |</a>
+        </td>
+        <td>
+            <a style="color: white" href="https://goo.gl/maps/1G3ebyVBuoo" target="_blank">| Adresse |</a>
+        </td>
+        <td></td>
+        <td>
+            Telefon: 0171 1980101
+        </td>
+    </table>
+
+</div>
+</html>
