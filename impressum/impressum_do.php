5f67bacf97d851c8c81ff1222d98045dbcdaa4c0
mg171 <mg171@hdm-stuttgart.de>
2018-01-31 18:26:01 +0100

    Reperatur Impressum

--- impressum/impressum_do.php
+++ impressum/impressum_do.php
@@ -23,21 +23,20 @@ $nachricht = $_POST['nachricht'];
 
 
 //Validieren
-if (empty($name)|| empty($besucher_mail))
+if (empty($name) && empty($besucher_mail))
 {
     echo "Bitte Name und Nachricht ausfüllen";
     exit;
 }
-
 $email_von = 'lenapopp8@gmail.com';
 $email_betreff = "Kontaktformular";
 $email_nachricht = "Ein Kontaktformular wurde auf Ihre Seite ausgefüllt von $name.\n".
-    "E-Mail Adresse: $besucher_mail\n".
-    "Nachricht:\n $nachricht".
+    "E-Mail Adresse: ".$besucher_mail."\n".
+    "Nachricht:\n".$nachricht;
 
 $to = 'lenapopp8@gmail.com';
 $headers = "Von: $email_von \r\n";
 
 //Mail schicken
-mail($to, $email_betreff, $email_nachricht, $headers);
+mail($to, $email_betreff, $email_nachricht, $headers, $email_von);
 
