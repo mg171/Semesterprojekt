64e96446a6adc1031e144192e53e68f9265226b1
mg171 <mg171@hdm-stuttgart.de>
2018-02-03 13:10:52 +0100

    uuml

--- mail/index.php
+++ mail/index.php
@@ -13,19 +13,19 @@
     $from .= "From: Handy Webshop <handy.webshop@gmail.com>\n";
     $from .= "Reply-To: handy.webshop@gmail.com\n";
     $from .= "Content-Type: text/html\n";
-    $text .= "<h1>Bestellbestätigung</h1> 
+    $text .= "<h1>Bestellbest&auml;tigung</h1> 
               <p>Vielen Dank für Ihre Bestellung! <br>
               Alle Preise verstehen sich inkl. 19% Mwst. <br>
               Lieferung erfolgt binnen 3 Werktagen nach Zahlungseingang. <br>
-              Wir erwarten Ihre Zahlung binnen von 10 Tagen per Überweisung oder Kreditkarte auf unser Konto. <br>
-              Überweisungen richten Sie bitte mit ihrer Bestellungnummer ".$newbestnr ." als Verwendungszweck an <br>
+              Wir erwarten Ihre Zahlung binnen von 10 Tagen per &Uuml;berweisung oder Kreditkarte auf unser Konto. <br>
+              &Uuml;berweisungen richten Sie bitte mit ihrer Bestellungnummer ".$newbestnr ." als Verwendungszweck an <br>
               <br>
               Bank: HdM-Bank <br>
               IBAN: DE12345678765432 <br>
               Inhaber: Webshop Handy <br>
               Betrag: ".$preisgesamt. "<br>
 
-              Mit freundlichen Grüßen, <br>
+              Mit freundlichen Gr&uuml;ßen, <br>
               Webshop Handy | Marcel, Monique und Lena
               </p>";
 
